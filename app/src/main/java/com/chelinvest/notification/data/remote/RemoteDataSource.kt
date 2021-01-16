5d14dca495192ecc93712cc77778f518eca0ae6d
alexandrios <alex27@mail.ru>
2021-01-16 21:50:07 +0500

    added log requestBodyText

--- app/src/main/java/com/chelinvest/notification/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/chelinvest/notification/data/remote/RemoteDataSource.kt
@@ -1,10 +1,12 @@
 package com.chelinvest.notification.data.remote
 
+import android.util.Log
 import com.chelinvest.notification.api.OkRequest
 import com.chelinvest.notification.api.request.LoginRequest
 import com.chelinvest.notification.api.request.MainRequest
 import com.chelinvest.notification.model.session.Session
 import com.chelinvest.notification.utils.Constants
+import com.chelinvest.notification.utils.Constants.LOG_TAG
 import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody
@@ -30,6 +32,8 @@ class RemoteDataSource @Inject constructor(private val remoteService: RemoteServ
         val stringWriter = StringWriter()
         serializer.write(request, stringWriter)
         val requestBodyText = stringWriter.toString().replace("+", "%2B")
+        Log.d(LOG_TAG, "requestBodyText=$requestBodyText")
+
 
         val ENCODING = "windows-1251"
         val mediaType =
